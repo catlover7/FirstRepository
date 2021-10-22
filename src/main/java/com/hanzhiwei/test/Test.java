ec812ff66bd17543c4ddee5e6afd405448e051c9
hzw <813382213@qq.com>
2021-10-22 16:42:39 +0800

    helloworld

---
+++ src/main/java/com/hanzhiwei/test/Test.java
@@ -0,0 +1,12 @@
+package com.hanzhiwei.test;
+
+/**
+ * @author 韩志伟
+ * @Description
+ * @create 2021-10-22-16:31
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
